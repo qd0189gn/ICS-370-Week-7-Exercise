2f20c578562329f2d3f4cf93d2f24b2572fa8b3a
Jyang232 <70220360+Jyang232@users.noreply.github.com>
2021-02-25 20:08:19 -0600

    Create MovieDriver.java

---
+++ MovieDriver.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class MovieDriver {
+	
+	public static void main(String[] args) {
+		
+		String varaible = "movie_id";
+		String table = "movies";
+		
+		String username = "root"; 
+		String password = " "; //*********enter your password here
+
+		try {
+			Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", username , password);
+			
+			java.sql.Statement myStat = myCon.createStatement();
+			
+			ResultSet myRs = myStat.executeQuery("select " + varaible + " from " + table);
+			
+			while(myRs.next())
+			{
+				System.out.println(myRs.getString(varaible));
+				}
+			}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			}
+		}
+	}
